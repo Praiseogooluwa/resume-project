229b5306482f31e909e87176f0ab07a13731eb07
Isaiah Praise Ogooluwa Bakare <92177420+Praiseogooluwa@users.noreply.github.com>
2025-07-19 16:52:14 -0400

    Delete api/index.py

--- api/index.py
+++
@@ -1,7 +0,0 @@
-from .main import app
-
-# This is required for Vercel
-def handler(event, context):
-    from mangum import Mangum
-    asgi_handler = Mangum(app)
-    return asgi_handler(event, context)
